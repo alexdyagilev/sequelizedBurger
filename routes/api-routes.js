e94d377dd5b6124bfd835610a0e30caf7e9cbbae
Alex Dyagilev <nosskyline@gmail.com>
2017-05-12 21:47:12 -0400

    getting there

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,22 +1,44 @@
 
-var db = require("../models");
+var Character = require("../models/burger.js");
 
+// Routes
+// =============================================================
 module.exports = function(app) {
 
+  app.get("/api/:burger?", function(req, res) {
 
-  app.get("/api/burger", function(req, res) {
+    if (req.params.characters) {
+
+      Burger.findOne({
+        where: {
+          routeName: req.params.characters
+        }
+      }).then(function(result) {
+        return res.json(result);
+      });
+    }
+
+    else {
+
+      Burger.findAll({})
+        .then(function(result) {
+          return res.json(result);
+        });
+    }
 
   });
 
-  app.post("/api/burger", function(req, res) {
+  app.post("/api/new", function(req, res) {
 
-  });
+    var burger = req.body;
 
-  app.delete("/api/burger/:id", function(req, res) {
+    var routeName = burger.name.replace(/\s+/g, "").toLowerCase();
 
-  });
-
-  app.put("/api/burger", function(req, res) {
+    Burger.create({
+      routeName: routeName,
+      name: burger.name,
+      devoured: burger.devoured,
+    });
 
   });
 };
